1c8147bb519276027e88dd5ef33b7c74618d85c4
Olyazarichna <olya.zarichna@gmail.com>
2022-09-21 14:57:36 +0300

    Update Form.js

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,26 +1,32 @@
 import { useState } from "react";
 import { findWord } from "../../services/api";
-export  const Form =()=>{
-    const [search, setSearch] = useState('');
 
-    const handleChange=(event)=>{
-      setSearch(event.currentTarget.value) ;
-    }
+export const Form = () => {
+  const [search, setSearch] = useState("");
 
-    const handleSubmit=(event)=>{
-        event.preventDefault();
-        findWord(search);
-        setSearch('');
-    }
-  
-    return(
-        <>
-    <form onSubmit={handleSubmit}>
-        <input type='search' placeholder='Search' autoFocus={true} onChange={handleChange} value={search}/>
+  const handleChange = (event) => {
+    setSearch(event.currentTarget.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    findWord(search);
+    setSearch("");
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Search"
+          autoFocus={true}
+          onChange={handleChange}
+          value={search}
+        />
         <button>Search</button>
-    </form>
-    
-        </>
-    )
-}
+      </form>
+    </>
+  );
+};
 export default Form;
