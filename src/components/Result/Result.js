30e884a39c2cfe8035b6418946615a294e7ad5a3
Olyazarichna <olya.zarichna@gmail.com>
2022-09-26 12:53:08 +0300

    search

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,29 +1,21 @@
-import {Meanings} from '../../Meanings/Meanings';
-import css from './Result.module.css';
-export const Result = ({data}) => {
-
-  return ( 
+import { Meanings } from "../../Meanings/Meanings";
+import css from "./Result.module.css";
+export const Result = ({ data }) => {
+  return (
     <>
       <div>
         {data ? (
           <>
             <h2 className={css.word}>{data.word}</h2>
             <p className={css.word}>{data.phonetic}</p>
-            {/* <ul> */}
-                 {data.meanings.map((meaning, index)=>{
-              return(
-                // <li key={index}>
-                  <Meanings key={index} meaning={meaning}/>
-                // </li>
-              )
+
+            {data.meanings.map((meaning, index) => {
+              return <Meanings key={index} meaning={meaning} />;
             })}
-            {/* </ul> */}
-        
-            </>
+          </>
         ) : (
-          <p>Search a word</p>
-        )
-        }
+          <p className={css.text}>Search a word</p>
+        )}
       </div>
     </>
   );
